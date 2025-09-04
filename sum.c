121b6088c7e8a90e55225af090d43655f70af23d
satyam262005 <satyam.cs241291@global.org.in>
2025-09-04 15:09:58 +0530

    loop

--- sum.c
+++ sum.c
@@ -1,4 +1,14 @@
 #include <stdio.h>
 int main(){
-
+int n=6;
+int t1=0;
+int t2=1;
+int next;
+for (int i=1;i<=n;i++){
+    printf("%d ",t1);
+    next=t1+t2;
+    t1=t2;
+    t2=next;
+}
+return 0;
 }
