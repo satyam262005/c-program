baf7ad793e030e36c139b02ec39ba71cfa1fbdb0
satyam262005 <satyam.cs241291@global.org.in>
2025-09-04 14:55:55 +0530

    test

---
+++ ispalin.cpp
@@ -0,0 +1,19 @@
+        #include<stdio.h>
+int main(){
+    int num=121;
+    int original=num;
+    int res=0;
+    while (num!=0){
+        int lastdigit=num%10;
+        res= res*10+ lastdigit;
+num=num/10;
+    }
+    if (original==res)
+    {
+        
+        printf("%d is palindrome.",res);
+    }
+    else{
+        printf("%d is not palindrome",res);
+    }
+}
