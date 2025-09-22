fa39898ae1f60793b2488d6b775be231009b2abc
satyam262005 <satyam.cs241291@global.org.in>
2025-09-22 15:39:08 +0530

    swape

--- pointer.cpp
+++ pointer.cpp
@@ -1,4 +1,4 @@
- #include<stdio.h>
+//  #include<stdio.h>
 //  int sum (int x,int y){
 // return x+y;
 //  }
@@ -9,18 +9,43 @@
 //     printf("%d", data);
 //  }
 
- int sum (int *x,int *y){
-    *x=12;
-    *y=13;
-    int s=(*x)+(*y);
-return s;
+//  int sum (int *x,int *y){
+//     *x=12;
+//     *y=13;
+//     int s=(*x)+(*y);
+// return s;
+//  }
+//  int main (){
+//     int x=1;
+//     int y=26;
+//     int data=sum (&x,&y);
+//     printf("%d",data);
+//     printf("%d",x+y);
+//  }
+
+
+// #include<stdio.h>
+// int main(){
+//     int arr[5]={1,2,3,4,5};
+//     int *p=arr;
+//     printf("%d",*(p));
+//      printf("%d",*(p+1));
+//       printf("%d",*(p+2));
+// }
+
+
+ #include<stdio.h>
+
+ int swape (int *x,int *y){
+    int temp =*x;
+  *x=*y;
+  *y=temp;
  }
  int main (){
-    int x=1;
-    int y=26;
-    int data=sum (&x,&y);
-    printf("%d",data);
-    printf("%d",x+y);
+    int x=12;
+    int y=13;
+    printf("x=%d, y=%d\n",x,y);
+        printf("x=%d, y=%d",y,x);
+
+    
  }
-
-
