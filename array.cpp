fe794361b6843f39f215b24042051e98b2aa5c32
satyam262005 <satyam.cs241291@global.org.in>
2025-09-23 15:02:08 +0530

    sum

--- array.cpp
+++ array.cpp
@@ -1,6 +1,6 @@
 #include<stdio.h>
 int main(){
-    int arr[3][3]={{1,2,3},{4,5,6}};
+    int arr[3][3]={{1,2,3},{4,5,6},{7,8,9}};
     for (int  i = 0; i < 3; i++)
     {
         for (int  j = 0; j < 3; j++)
@@ -9,5 +9,18 @@ int main(){
         }printf("\n");
         
     }
-    
+
+int sum=0;
+for (int  i = 0; i < 3; i++){
+    {
+        for (int  j = 0; j < 3; j++)
+        sum=sum+arr[i][j];
+        
+    }
+
+
+}
+printf("sum=%d",sum);
+int max=arr[0][0];
+return 0;
 }
