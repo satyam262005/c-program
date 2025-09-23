2a6ed0440c28bcbd0cfa16ae8963645bf2e0aeba
satyam262005 <satyam.cs241291@global.org.in>
2025-09-23 15:21:24 +0530

    1d array

--- array.cpp
+++ array.cpp
@@ -1,26 +1,41 @@
-#include<stdio.h>
-int main(){
-    int arr[3][3]={{1,2,3},{4,5,6},{7,8,9}};
-    for (int  i = 0; i < 3; i++)
+// #include <stdio.h>
+// int main()
+// {
+//     int arr[3][3] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+//     for (int i = 0; i < 3; i++)
+//     {
+//         for (int j = 0; j < 3; j++)
+//         {
+//             printf("%d", arr[i][j]);
+//         }
+//         printf("\n");
+//     }
+
+//     int sum = 0;
+//     for (int i = 0; i < 3; i++)
+//     {
+//         for (int j = 0; j < 3; j++)
+//         {
+//             sum = sum + arr[i][j];
+//         }
+    
+//     }
+//     printf("sum=%d", sum);
+//     int max = arr [0][0];
+//     return 0;
+// }
+
+// C program to illustrate how to create an array,
+// initialize it, update and access elements
+#include <stdio.h>
+int main()
+{
+    int arr[3][3] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+    for (int i = 0; i < 3; i++)
     {
-        for (int  j = 0; j < 3; j++)
+        for (int j = 0; j < 3; j++)
         {
-            printf("%d",arr[i][j]);
-        }printf("\n");
-        
+            printf("%d ", arr[i][j]);
+        }
     }
-
-int sum=0;
-for (int  i = 0; i < 3; i++){
-    {
-        for (int  j = 0; j < 3; j++)
-        sum=sum+arr[i][j];
-        
-    }
-
-
-}
-printf("sum=%d",sum);
-int max=arr[0][0];
-return 0;
 }
