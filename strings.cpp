2e6505beeb994335b281ae0d7b17755f0564b2c6
satyam262005 <satyam.cs241291@global.org.in>
2025-10-08 15:21:43 +0530

    compress

--- strings.cpp
+++ strings.cpp
@@ -19,6 +19,25 @@
 
 
 
+// #include <stdio.h>
+// #include <string.h>
+// int main()
+// {
+//     char str[] = "abc";
+//     int freq[256] = {0};
+//     int ln = strlen(str);
+//     for (int i = 0; i < ln; i++)
+//     {
+//         freq[str[i]]++;
+//     }
+//     for (int i = 0; i < 256; i++){
+//     if (freq[i] == 1)
+//     {
+//         printf("%c:\n", i, freq[i]);
+//     }
+// }
+// }
+
 #include <stdio.h>
 #include <string.h>
 int main()
@@ -31,9 +50,9 @@ int main()
         freq[str[i]]++;
     }
     for (int i = 0; i < 256; i++){
-    if (freq[i] == 1)
+    if (freq[i]>0)
     {
-        printf("%c:\n", i, freq[i]);
+        printf("%c%d", i, freq[i]);
     }
 }
 }
